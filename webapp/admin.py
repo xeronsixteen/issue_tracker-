0e33227a8bc2b2c7414c3e65a002d505c644cf4d
Murat Alaichiev <murat.alaichiev@gmail.com>
2022-07-12 17:16:57 +0600

    updated admin.py with registration new field TaskType

--- webapp/admin.py
+++ webapp/admin.py
@@ -1,18 +1,19 @@
 from django.contrib import admin
 
 # Register your models here.
-from webapp.models import Task, Type, Status
+from webapp.models import Task, Type, Status, TaskType
 
 
 class TaskAdmin(admin.ModelAdmin):
-    list_display = ['id', 'description', 'status', 'created_at', 'updated_at', 'type', 'summary']
+    list_display = ['id', 'summary', 'description', 'status', 'created_at', 'type']
     list_display_links = ['summary']
     list_filter = ['status']
-    search_fields = ['description', 'status']
+    search_fields = ['summary', 'status']
     fields = ['summary', 'description', 'status', 'type']
-    # readonly_fields = ['Date']
+    readonly_fields = ['created_at', 'updated_at']
 
 
-admin.site.register(Task, TaskAdmin)
+admin.site.register(Task)
 admin.site.register(Type)
 admin.site.register(Status)
+admin.site.register(TaskType)
