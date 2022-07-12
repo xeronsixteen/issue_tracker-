52525b642a89a91063ecd3cce6b4b9adde5ad509
Murat Alaichiev <murat.alaichiev@gmail.com>
2022-07-12 17:18:16 +0600

    updated type Form with Multiple Choice option + description field's required attr is become False

--- webapp/forms.py
+++ webapp/forms.py
@@ -1,13 +1,12 @@
 from django import forms
 from django.forms import widgets
 
-from webapp.models import Type, Status
+from webapp.models import Type, Status, Task
 
 
 class TaskForm(forms.Form):
-    summary = forms.CharField(max_length=50, required=True, label='Name')
-    description = forms.CharField(max_length=500, required=True, label='Text', widget=widgets.Textarea(attrs={"cols":
-                                                                                                                  40,
-                                                                                                      "rows": 3}))
-    type = forms.ModelChoiceField(queryset=Type.objects.all())
+    summary = forms.CharField(max_length=50, required=True, label='Summary')
+    description = forms.CharField(max_length=500, required=False, label='Description',
+                                  widget=widgets.Textarea(attrs={"cols": 40, "rows": 3}))
+    type = forms.ModelMultipleChoiceField(queryset=Type.objects.all(), required=True, label='Type')
     status = forms.ModelChoiceField(queryset=Status.objects.all())
