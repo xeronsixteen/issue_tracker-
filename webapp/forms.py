7e6fd064b0aa7f0a15e22527d5c735ab79e977f0
Murat Alaichiev <murat.alaichiev@gmail.com>
2022-07-14 15:05:45 +0600

    create three validation functions: 1) clean method to summary: checker for length and letteronlymode, checker for existence of same titles 2) clean method to description: checker for length, checker for existence of same description texts

--- webapp/forms.py
+++ webapp/forms.py
@@ -1,4 +1,6 @@
+import re
 from django import forms
+from django.core.exceptions import ValidationError
 from django.forms import widgets
 
 from webapp.models import Task
@@ -10,6 +12,29 @@ class TaskForm(forms.ModelForm):
         fields = '__all__'
         widgets = {
             'type': widgets.CheckboxSelectMultiple,
-            'description': widgets.Textarea(attrs={"placeholder": 'Input full description of your task'})
+            'description': widgets.Textarea(attrs={"placeholder": 'Input full description of your task'}),
+            'status': widgets.RadioSelect
         }
 
+    def clean_summary(self):
+        summary = self.cleaned_data.get('summary')
+        if len(summary) <= 4:
+            raise ValidationError('Please, enter more than 4 symbols')
+        if Task.objects.filter(summary=summary).exists():
+            raise ValidationError('A task with that name exists')
+        if not re.match('ˆ[a-zA-Zа-яА-Я\s]+$', summary):
+            raise ValidationError ('Please, enter only letters')
+        return summary
+
+    def clean_description(self):
+        description = self.cleaned_data.get('description')
+        if len(description) > 2000:
+            raise ValidationError('Max length of description is 2000')
+        if Task.objects.filter(description=description).exists():
+            raise ValidationError('A description with that name exists')
+        return description
+
+    def clean(self):
+        if self.cleaned_data.get('summary') == self.cleaned_data.get('description'):
+            raise ValidationError('Summary and description could not be the same')
+        return super().clean()
