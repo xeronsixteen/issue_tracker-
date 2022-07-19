ac4cd023eab957d39a32bc93481ef4a8a5fb92c9
Murat Alaichiev <murat.alaichiev@gmail.com>
2022-07-19 18:52:07 +0600

    Created requests.py where I checked all options from HomeWork52

---
+++ webapp/requests.py
@@ -0,0 +1,24 @@
+from webapp.models import Task, Type, Status
+from datetime import datetime, timedelta
+from django.db.models import Q, F
+
+
+#first task
+Task.objects.filter(created_at__gte=datetime.now()-timedelta(days=30)).filter(status__name__istartswith='Done')
+
+#second_task
+q1 = Q(status__name__in=('New', 'In Progress'))
+q2 = Q(type__name__in=('Bug', 'Task'))
+Task.objects.filter(q1 & q2)
+
+#third_task
+q3 = Q(type__name='Bug')
+q4 = Q(status__name='New')
+q5 = Q(status__name='In Progress')
+Task.objects.filter(q3 | q4 | q5)
+
+
+
+
+
+
