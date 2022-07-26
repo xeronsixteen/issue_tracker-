a697bfb11ffa55b98a15aa04c6533625100e726c
Murat Alaichiev <murat.alaichiev@gmail.com>
2022-07-26 18:43:19 +0600

    Updated urls after creation of Project

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
 
-from webapp.views import IndexView, TaskView, CreateView, UpdateView, DeleteView
+from webapp.views import IndexView, TaskView, UpdateView, DeleteView, CustomCreateView
+from webapp.views.projects import ProjectView, OneProjectView, CreateProject
 
 urlpatterns = [
-    path('', IndexView.as_view(), name="index"),
+    path('tasks/', IndexView.as_view(), name="task_list"),
     path('task/<int:pk>/', TaskView.as_view(), name="task_view"),
-    # path('tasks/', RedirectView.as_view(pattern_name="index")),
-    path('tasks/add/', CreateView.as_view(), name="create"),
+    path('tasks/add/', CustomCreateView.as_view(), name="create"),
     path('task/<int:pk>/update/', UpdateView.as_view(), name="update"),
     path('task/<int:pk>/delete/', DeleteView.as_view(), name="delete"),
+    path('', ProjectView.as_view(), name='project_list'),
+    path('project/<int:pk>/', OneProjectView.as_view(), name='project_view'),
+    path('project/add/', CreateProject.as_view(), name="create_project"),
 ]
