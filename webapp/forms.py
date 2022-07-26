c10e15b07aff4147900f215979023832cbca4f9a
Murat Alaichiev <murat.alaichiev@gmail.com>
2022-07-26 18:42:57 +0600

    created in forms.py new form for Project

--- webapp/forms.py
+++ webapp/forms.py
@@ -3,7 +3,7 @@ from django import forms
 from django.core.exceptions import ValidationError
 from django.forms import widgets
 
-from webapp.models import Task
+from webapp.models import Task, Project
 
 
 class TaskForm(forms.ModelForm):
@@ -42,3 +42,10 @@ class TaskForm(forms.ModelForm):
 
 class SearchForm(forms.Form):
     search = forms.CharField(max_length=50, required=False, label='Find')
+
+
+class ProjectForm(forms.ModelForm):
+    class Meta:
+        model = Project
+        fields = '__all__'
+
