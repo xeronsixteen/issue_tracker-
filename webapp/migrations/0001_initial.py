b3ffe70a24c582d1b4ade7a7f861a202804b460f
Murat Alaichiev <murat.alaichiev@gmail.com>
2022-07-26 18:41:43 +0600

    refactortored initial py in migration file, it is hard to explain why this file was edited by itself (Kirill)

--- webapp/migrations/0001_initial.py
+++ webapp/migrations/0001_initial.py
@@ -25,7 +25,7 @@ class Migration(migrations.Migration):
             },
         ),
         migrations.CreateModel(
-            name='tasks',
+            name='Task',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('created_at', models.DateTimeField(auto_now_add=True, verbose_name='date of creation')),
