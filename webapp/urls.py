626ed88abbce8854d0e39429d2aaabb26ea02b4d
Murat Alaichiev <murat.alaichiev@gmail.com>
2022-07-29 16:59:43 +0600

    rename all links in urls.py according to classes in views

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
 
-from webapp.views import IndexView, TaskView, UpdateView, DeleteView, CustomCreateView
+from webapp.views import IndexView, TaskView, TaskUpdateView, TaskCreateView, TaskDeleteView
 from webapp.views.projects import ProjectView, OneProjectView, CreateProject, UpdateProject, DeleteProject
 
 urlpatterns = [
     path('tasks/', IndexView.as_view(), name="task_list"),
     path('task/<int:pk>/', TaskView.as_view(), name="task_view"),
-    path('tasks/add/', CustomCreateView.as_view(), name="create"),
-    path('task/<int:pk>/update/', UpdateView.as_view(), name="update"),
-    path('task/<int:pk>/delete/', DeleteView.as_view(), name="delete"),
+    path('tasks/add/', TaskCreateView.as_view(), name="create"),
+    path('task/<int:pk>/update/', TaskUpdateView.as_view(), name="update"),
+    path('task/<int:pk>/delete/', TaskDeleteView.as_view(), name="delete"),
     path('', ProjectView.as_view(), name='project_list'),
     path('project/<int:pk>/', OneProjectView.as_view(), name='project_view'),
     path('project/add/', CreateProject.as_view(), name="create_project"),
