65aa0eb63ff718d0224ce4b0ad13fd127e6359d0
Murat Alaichiev <murat.alaichiev@gmail.com>
2022-08-05 18:12:58 +0600

    Created new app 'accounts', WHERE:
    1) created urls.py, WHERE registered path for logout and login
    2) created login.html

---
+++ accounts/urls.py
@@ -0,0 +1,10 @@
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import path
+
+app_name = 'accounts'
+
+urlpatterns = [
+    path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+
+]
