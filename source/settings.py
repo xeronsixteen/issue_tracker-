9d39f8618e9e9c48dc9f842bf2d24e62a0f91822
Murat Alaichiev <murat.alaichiev@gmail.com>
2022-08-05 18:10:16 +0600

    Added Login panel to application:
    Adde in settings.py and urls.py
    - LOGIN URL, REDIRECTS in Logout | Login
    - in INSTALLED_APP registered 'accounts'
    - in urls.py registered new path

--- source/settings.py
+++ source/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'webapp'
+    'webapp',
+    'accounts'
 ]
 
 MIDDLEWARE = [
@@ -118,6 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+LOGIN_URL = 'accounts:login'
+LOGIN_REDIRECT_URL = 'webapp:project_list'
+LOGOUT_REDIRECT_URL = 'webapp:project_list'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
