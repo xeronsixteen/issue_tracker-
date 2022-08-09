b500c2810a91b12a8c06632d334fb33edadba5e3
Murat Alaichiev <murat.alaichiev@gmail.com>
2022-08-09 17:42:57 +0600

    in views.py
    created new class of registration new user with methods: get success url and form valid
    
    in templates/registration
    
    created new registration.html with standard form

---
+++ accounts/views.py
@@ -0,0 +1,27 @@
+from django.contrib.auth import login
+from django.contrib.auth.models import User
+from django.shortcuts import redirect
+from django.urls import reverse
+from django.views.generic import CreateView
+
+from webapp.forms import MyUserCreationForm
+
+
+class RegisterView(CreateView):
+    model = User
+    template_name = 'registration/registration.html'
+    form_class = MyUserCreationForm
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect(self.get_success_url())
+
+    def get_success_url(self):
+        next_url = self.request.GET.get('next')
+        if not next_url:
+            next_url = self.request.POST.get('next')
+        if not next_url:
+            next_url = reverse('webapp:task_list')
+
+        return next_url
