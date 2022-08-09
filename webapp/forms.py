5e2ce1fea82b6ef881ec4960ac53261a8f365c18
Murat Alaichiev <murat.alaichiev@gmail.com>
2022-08-09 17:32:23 +0600

    in forms.py
    - created new ModelForm for Creation of User with validators of existing email, username name and surname

--- webapp/forms.py
+++ webapp/forms.py
@@ -1,6 +1,8 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
-from django.forms import widgets
+from django.forms import widgets, EmailField
 
 from webapp.models import Task, Project
 
@@ -85,3 +87,31 @@ class ProjectForm(forms.ModelForm):
             'created_at': widgets.SelectDateWidget,
             'finished_at': widgets.SelectDateWidget,
         }
+
+
+class MyUserCreationForm(UserCreationForm):
+    email = EmailField(required=True)
+
+    class Meta(UserCreationForm.Meta):
+        fields = ['username', 'password1', 'password2',
+                  'first_name', 'last_name', 'email']
+
+    def clean_email(self):
+        email = self.cleaned_data['email'].strip()
+        if User.objects.filter(email__iexact=email).exists():
+            raise ValidationError('Такое мыло существует в БД')
+        return email
+
+    def clean_username(self):
+        username = self.cleaned_data['username'].strip()
+        if User.objects.filter(username__iexact=username).exists():
+            raise ValidationError('Такое имя существует в БД')
+        return username
+
+    def clean(self):
+        cleaned_data = super().clean()
+        first_name = cleaned_data['first_name'].strip()
+        last_name = cleaned_data['last_name'].strip()
+        if not first_name and not last_name:
+            raise ValidationError('Заполните хотя бы одно из полей : имя или фамилия')
+
