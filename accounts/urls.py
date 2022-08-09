dad34c7c6bd9f7ef40a9a7f0004438f4ec4daa8e
Murat Alaichiev <murat.alaichiev@gmail.com>
2022-08-09 17:34:47 +0600

    in accounts urls:
    - registered new path of registration button

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,13 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
+from accounts.views import RegisterView
+
 app_name = 'accounts'
 
 urlpatterns = [
     path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path('registration/', RegisterView.as_view(), name='registration')
 
 ]
