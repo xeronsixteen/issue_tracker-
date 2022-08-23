19db947c6bcad06098815a2046c5624c68444df5
Murat Alaichiev <murat.alaichiev@gmail.com>
2022-08-23 13:52:45 +0600

    in urls.py of accounts
    added path to users

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,13 +1,14 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
-from accounts.views import RegisterView
+from accounts.views import RegisterView, UsersView
 
 app_name = 'accounts'
 
 urlpatterns = [
     path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
-    path('registration/', RegisterView.as_view(), name='registration')
+    path('registration/', RegisterView.as_view(), name='registration'),
+    path('users/', UsersView.as_view(), name='UsersView')
 
 ]
